032f444650bcab2a2fb49bde7b664c499f8d9f5a
lai-kc <116620625+lai-kc@users.noreply.github.com>
2022-10-25 13:17:49 +0800

    Update example.py

--- example.py
+++ example.py
@@ -4,22 +4,24 @@ Created on Tue Oct 25 11:13:59 2022
 
 @author: User
 """
-
+#to define factorial
 def fact(n):
     a=1
     for i in range (2,n+1):
        a=a*i 
     return a
-
+#define term n for arcsin
 def t(x,n):
     return (fact(2*n))/(2**(2*n))/(fact(n)**2)/(2*n+1)*(x**(2*n+1))
 
+#calculate pi using arcsin for for 30 terms
 x=0.5
 a=0
 for n in range (0,30):
     a=a+t(x,n)
     print(6*a)
 
+#calc pi with precision 0.001
 a=0   
 dif=1  
 n=0  
